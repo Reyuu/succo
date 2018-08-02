61182440794e51b50e4dd892fd51b5868e81af00
Reyuu <rey444xd3@gmail.com>
2018-08-02 23:56:25 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.get("/", function(req, res){
+    res.send(req.url);
+    res.end();
+});
+
+app.post("/", urlencodedParser, function(req, res){
+    res.send(r.body);
+    res.end();
+});
+
+var server = app.listen(8081, function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Listening at http://%s:%s", host, port);
+});
