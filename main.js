f67064310899e380ee8909dc7c621ceefde1cbeb
Reyuu <rey444xd3@gmail.com>
2018-08-03 00:14:12 +0000

    added console logging

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.get("/", function(req, res){
     res.send(req.url);
+    console.log(req.url);
     res.end();
 });
 
 app.post("/", urlencodedParser, function(req, res){
     res.send(r.body);
+    console.log(req.body);
     res.end();
 });
 
