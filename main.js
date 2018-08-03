8224b357742be965e7cffc8e9ea5c2022555755d
Reyuu <rey444xd3@gmail.com>
2018-08-03 04:37:43 +0000

    added https

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var https = require("https");
+var fs = require('fs');
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get("/", function(req, res){
+app.get("*", function(req, res){
     res.send(req.url);
     console.log(req.url);
     res.end();
 });
 
-app.post("/", urlencodedParser, function(req, res){
+app.post("*", urlencodedParser, function(req, res){
     res.send(r.body);
     console.log(req.body);
     res.end();
 });
 
-var server = app.listen(8081, function(){
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Listening at http://%s:%s", host, port);
-});
+var options = {
+    key: fs.readFileSync("cert/cert.key"),
+    cert: fs.readFileSync("cert/cert.crt")
+};
+
+//Because we're listening on low port, need to run with su/Admin
+https.createServer(options, app).listen(443);
